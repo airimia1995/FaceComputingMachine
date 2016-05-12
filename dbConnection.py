484c90561e9018b156cd55b75036fa0b8fe8e4ac
Anurag Bhardwaj <anurag.wizard@gmail.com>
2016-05-12 00:16:57 -0700

    Minor changes

--- dbConnection.py
+++ dbConnection.py
@@ -13,7 +13,7 @@ reload(sys);
 sys.setdefaultencoding("utf-8");
 
 def connectToDatabase():
-	conn_string = "dbname='sunedison' port='5439' user='abhardwaj' password='Master12' host='sunedisondatawarehouse.cgnr3c8sn1sz.us-west-2.redshift.amazonaws.com'";
+	conn_string = "dbname='sunedison' port='5439' user='XXXX' password='XXXX' host='sunedisondatawarehouse.cgnr3c8sn1sz.us-west-2.redshift.amazonaws.com'";
 	print "Connecting to Redshift";
 	conn = psycopg2.connect(conn_string);
 	print "Connected to Redshift";
